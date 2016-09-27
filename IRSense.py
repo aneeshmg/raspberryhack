3ae682f10292319bbcfbf95023be1ce6e50593e4
aneeshmg <aneesh.mg2007@gmail.com>
2016-09-27 13:00:47 +0530

    updated - done

--- IRSense.py
+++ IRSense.py
@@ -1,22 +1,74 @@
-import RPi.GPIO as GPIO
-import time
+''' Imports '''
 
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import sleep
+import json
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import time
+from picamera import PiCamera
+from subprocess import call
+
+''' Imports done '''
+
+''' Class definitions '''
+
+class Config(object):
+    def __init__(self, jsonData):
+        self.__dict__ = json.loads(jsonData)
+
+
+''' Config setup done '''
+
+''' emailer '''
+
+def SendMail(ImageFile):
+    call(["mpack", "-s", c.basicSubject, "-d", c.TextMessageFile, ImageFile, c.email])
+
+''' emailer done '''
+
+def getLatestImageId():
+    imgs = os.listdir(c.storage)
+    return str(len(imgs))
+
+def TakePic():
+    latestIndex = getLatestImageId()
+    latestIndex = int(latestIndex)
+    latestIndex = latestIndex + 1
+    camera.capture(c.storage + str(latestIndex) + ".jpg")
+
+def getLatestImage():
+    return c.storage + getLatestImageId() + ".jpg"
+
+''' Initialisation '''
+
+camera = PiCamera()
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.IN)
 GPIO.setup(3,GPIO.OUT)
+configJson = open('config.json', 'r').read()
+c = Config(configJson)
+
+''' Initialisation ends '''
 
 while True:
     i=GPIO.input(11)
+
     if i==0:
-        
            print "No intruders",i
            GPIO.output(3,0)
-           time.sleep(0.1)
+           time.sleep(c.frequency)
 
 
     elif i==1:
-           print "Intruder detected",i
-           GPIO.output(3,1)
-           time.sleep(0.1)
-           
+            print "Intruder detected",i
+            TakePic()
+            SendMail(getLatestImage())
+            GPIO.output(3,1)
+            time.sleep(c.frequency)
+
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 ''' Imports '''
 
+import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
 import json
@@ -8,9 +9,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+import time
+from picamera import PiCamera
 from subprocess import call
+import subprocess
 
 ''' Imports done '''
+
 ''' Class definitions '''
 
 class Config(object):
@@ -18,16 +23,15 @@ class Config(object):
         self.__dict__ = json.loads(jsonData)
 
 
-configJson = open('config.json', 'r').read()
-
-c = Config(configJson)
-
 ''' Config setup done '''
 
 ''' emailer '''
 
 def SendMail(ImageFile):
     call(["mpack", "-s", c.basicSubject, "-d", c.TextMessageFile, ImageFile, c.email])
+    ''' For async call
+    subprocess.Popen(["mpack", "-s", c.basicSubject, "-d", c.TextMessageFile, ImageFile, c.email])
+    '''
 
 ''' emailer done '''
 
@@ -39,23 +43,39 @@ def TakePic():
     latestIndex = getLatestImageId()
     latestIndex = int(latestIndex)
     latestIndex = latestIndex + 1
-    camera = PiCamera()
     camera.capture(c.storage + str(latestIndex) + ".jpg")
 
 def getLatestImage():
     return c.storage + getLatestImageId() + ".jpg"
 
-'''SendMail(c.storage + getLatestImage())'''
-TakePic()
-SendMail(getLatestImage())
+def MailSent():
+    print "Mail sent to : " + c.email
+
+''' Initialisation '''
+
+camera = PiCamera()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.IN)
+GPIO.setup(3,GPIO.OUT)
+configJson = open('config.json', 'r').read()
+c = Config(configJson)
+
+''' Initialisation ends '''
+
+while True:
+    i=GPIO.input(11)
+
+    if i==0:
+           print "No intruders",i
+           GPIO.output(3,0)
+           time.sleep(c.frequency)
 
 
+    elif i==1:
+            print "Intruder detected",i
+            TakePic()
+            SendMail(getLatestImage())
+            GPIO.output(3,1)
+            time.sleep(c.frequency)
 
-"""
-
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
-
-"""
